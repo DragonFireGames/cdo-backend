bffd2f464625816cac8e3fd34590d7e3cd7f5efa
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-01-28 10:32:47 -0800

    Add bcrypt, fix security

--- index.js
+++ index.js
@@ -152,8 +152,11 @@ app.get('/youtube/info/:id', async (req, res) => {
 //MD5 Hash - Ron Rivest
 !function(){function j(r) {var o, e, n, f = [ -680876936, -389564586, 606105819, -1044525330, -176418897, 1200080426, -1473231341, -45705983, 1770035416, -1958414417, -42063, -1990404162, 1804603682, -40341101, -1502002290, 1236535329, -165796510, -1069501632, 643717713, -373897302, -701558691, 38016083, -660478335, -405537848, 568446438, -1019803690, -187363961, 1163531501, -1444681467, -51403784, 1735328473, -1926607734, -378558, -2022574463, 1839030562, -35309556, -1530992060, 1272893353, -155497632, -1094730640, 681279174, -358537222, -722521979, 76029189, -640364487, -421815835, 530742520, -995338651, -198630844, 1126891415, -1416354905, -57434055, 1700485571, -1894986606, -1051523, -2054922799, 1873313359, -30611744, -1560198380, 1309151649, -145523070, -1120210379, 718787259, -343485551 ], t = [ o = 1732584193, e = 4023233417, ~o, ~e ], c = [], a = unescape(encodeURI(r)) + "\u0080", d = a.length;for (r = --d / 4 + 2 | 15, c[--r] = 8 * d; ~d; ) c[d >> 2] |= a.charCodeAt(d) << 8 * d--;for (i = a = 0; i < r; i += 16){for (d = t; 64 > a; d = [ n = d[3], o + ((n = d[0] + [ o & e | ~o & n, n & o | ~n & e, o ^ e ^ n, e ^ (o | ~n) ][d = a >> 4] + f[a] + ~~c[i | 15 & [ a, 5 * a + 1, 3 * a + 5, 7 * a ][d]]) << (d = [ 7, 12, 17, 22, 5, 9, 14, 20, 4, 11, 16, 23, 6, 10, 15, 21 ][4 * d + a++ % 4]) | n >>> -d), o, e ]) o = 0 | d[1], e = d[2];for (a = 4; a; ) t[--a] += d[a]}for (r = ""; 32 > a; ) r += (t[a >> 3] >> 4 * (1 ^ a++) & 15).toString(16);return r}(global.md5=j)}();
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://dragonfire7z:"+process.env['MONGODB_PASSWORD']+"@cdo-backend.sredm6y.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://dragonfire7z:"+process.env.MONGODB_PASSWORD+"@cdo-backend.sredm6y.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const mongocl = new MongoClient(uri, {
@@ -238,6 +241,7 @@ function randomId(len,alphabet) {
 
 // FPROF
 var accountData = {};
+var publicAccData = {};
 var sessionCache = {};
 var authtokens = {};
 var admintokens = {};
@@ -247,7 +251,7 @@ function giveToken(name) {
   return {
     tok:tok,
     name:name,
-    data:accountData[name]
+    data:publicAccData[name]
   };
 }
 var profapi = createAPI("prof");
@@ -255,16 +259,25 @@ profapi.ondbload = async ()=>{
   accountData = await profapi.get(0,{});
 };
 async function saveAccData() {
+  publicAccData = JSON.parse(JSON.stringify(accountData));
+  for (var i in publicAccData) {
+    delete publicAccData[i].credentials;
+  }
   return await profapi.save(0,accountData);
 }
 profapi.on('signup', async (data) => {
   var name = data.name.replace(/[^\w\d_-]/g,"").toLowerCase();
-  var cred = md5(data.cred);
+  //var cred = md5(data.cred);
+  var cred = await bcrypt.hash(data.cred, saltRounds);
   var uid = md5(data.uid);
   if (accountData[name] !== undefined) throw "Signup Failed: Account already exists";
   accountData[name] = {
     name: data.name,
     credentials: cred,
+    avatar: "default",
+    bio: "",
+    friends: [],
+    pending: [],
   };
   var def = data.data;
   for (var i in def) {
@@ -277,10 +290,11 @@ profapi.on('signup', async (data) => {
 });
 profapi.on('signin', async (data) => {
   var name = data.name.toLowerCase();
-  var cred = md5(data.cred);
   var uid = md5(data.uid);
   if (!accountData[name]) throw "Login Failed: Account doesn't exist";
-  if (accountData[name].credentials !== cred) throw "Login Failed: Invalid credentials";
+  //var credvalid = accountData[name].credentials === md5(data.cred);
+  var credvalid = await bcrypt.compare(data.cred, accountData[name].credentials);
+  if (!credvalid) throw "Login Failed: Invalid credentials";
   var def = data.data;
   for (var i in def) {
     if (accountData[name][i]) continue;
@@ -320,21 +334,23 @@ profapi.on('update', async (data) => {
   // Update
   var update = JSON.parse(data.data);
   for (var i in update) {
+    if (i == "cred") continue;
     accountData[name][i] = update[i];
     if (data[i] === null) delete accountData[name][i];
   }
   saveAccData();
-  return accountData[name];
+  return publicAccData[name];
 });
 profapi.on('delete', async (data) => {
-  var cred = md5(data.cred);
   var uid = md5(data.uid);
   var tok = data.tok;
   // Check
   var name = sessionCache[uid];
   var name2 = authtokens[tok];
-  if (!name || !name2 || name != name2) throw "Error: Not authenticated";
-  if (cred != accountData[name].credentials) throw "Error: Invalid Credentials";
+  if (!name || !name2 || name != name2) throw "Error: Not Authenticated";
+  //var credvalid = accountData[name].credentials === md5(data.cred);
+  var credvalid = await bcrypt.compare(data.cred, accountData[name].credentials);
+  if (!credvalid) throw "Error: Invalid Credentials";
   // Delete
   delete accountData[name];
   saveAccData();
@@ -349,19 +365,18 @@ profapi.on('delete', async (data) => {
 });
 profapi.on('get', async (name) => {
   // Check
-  var data = accountData[name];
-  if (!data) throw "Error: Nonexistent user"
-  return data;
+  var acc = publicAccData[name];
+  if (!acc) throw "Error: Nonexistent user"
+  return acc;
 });
 profapi.on('getall', async () => {
-  return accountData;
+  return publicAccData;
 });
-const adminpswd = md5(process.env['ADMIN_PASSWORD']);
+const adminpswd = process.env.ADMIN_PASSWORD;
 profapi.on('admin/elevate', async (data) => {
   var tok = data.tok;
   if (!authtokens[tok]) throw "Error: No valid token";
-  var cred = md5(data.cred);
-  if (cred != adminpswd) throw "Error: Invalid credentials";
+  if (data.cred !== adminpswd) throw "Error: Invalid credentials";
   admintokens[tok] = true;
   return "Successfully became admin";
 });
