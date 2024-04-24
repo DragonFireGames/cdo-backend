8e19ba8d3456389ed045ab626a8eba4c0ffba3bb
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-04-24 20:51:53 +0000

    fixrandom

--- index.js
+++ index.js
@@ -890,7 +890,7 @@ app.get("/unzip", async function(req, res) {
   var blob = await fetch(url).then(e=>e.blob());
   var buf = await blob.arrayBuffer();
   buf = Buffer.from(buf);
-  var id = randomId(8);
+  var id = Math.floor(Math.random()*1e12);
   var path = "cache/"+id+".7z";
   var dir = "cache/"+id;
   await fsp.writeFile(path,buf);
